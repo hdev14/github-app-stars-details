d5785c7083cc170b117a05299512c3f3e87a89a5
HermersonDev <ne.hermerson@gmail.com>
2020-03-10 15:45:27 -0300

    :construction: adds request for github starred repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -72,7 +72,7 @@ export default class Main extends Component {
 
   handleNavigate = user => {
     const {navigation} = this.props;
-    navigation.navigate('User', {user});
+    navigation.push('User', {user});
   };
 
   render() {
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,25 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import api from '../../services/api';
 
 export default class User extends Component {
+  static navigationOptions = ({route}) => ({
+    title: route.params.user.name,
+  });
+
+  state = {
+    stars: [],
+  };
+  async componentDidMount() {
+    const {user} = this.props.route.params;
+    console.tron.log(user);
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({
+      stars: response.data,
+    });
+  }
+
   render() {
     return (
       <View>
