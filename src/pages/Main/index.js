f1cd12cf666cb1623030bd86242f2ba50364b7e2
HermersonDev <ne.hermerson@gmail.com>
2020-03-10 11:58:18 -0300

    :construction: creates list of github users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,51 @@
 import React, {Component} from 'react';
+import {Keyboard} from 'react-native';
 import Icons from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 // Styles
 import Container from '../../styles/components/Container';
-import {Form, Input, SubmitButton} from './styles';
+import {
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const {newUser, users} = this.state;
+    console.tron.log(newUser);
+    const response = await api.get(`/users/${newUser}`);
+    const user = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, user],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
   render() {
+    const {newUser, users} = this.state;
     return (
       <Container>
         <Form>
@@ -14,11 +53,30 @@ export default class Main extends Component {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Adicione um usuário"
+            onChangeText={text => this.setState({newUser: text})}
+            value={newUser}
+            onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton>
+          <SubmitButton onPress={this.handleAddUser}>
             <Icons name="add" color="#fff" size={20} />
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({item}) => (
+            <User>
+              <Avatar source={{uri: item.avatar}} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -20,10 +20,60 @@ export const Input = styled.TextInput.attrs({
 `;
 
 export const SubmitButton = styled(RectButton)`
-  margin-left: 15px;
+  margin-left: 10px;
   border-radius: 5px;
   background-color: #333;
   justify-content: center;
   align-items: center;
   padding: 10px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  height: 64px;
+  width: 64px;
+  border-radius: 32px;
+  background-color: #ddd;
+`;
+
+export const Name = styled.Text`
+  margin-top: 5px;
+  font-size: 20px;
+  color: #333;
+  text-align: center;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLine: 2,
+})`
+  margin-top: 5px;
+  font-size: 14px;
+  color: #999;
+  line-height: 16px;
+  text-align: center;
+`;
+
+export const ProfileButton = styled(RectButton)`
+  align-self: stretch;
+  background-color: #333;
+  margin-top: 20px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  padding: 5px 0;
+`;
+
+export const ProfileButtonText = styled.Text`
+  font-size: 16px;
+  color: #fff;
 `;
